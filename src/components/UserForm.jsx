91870ee25c5471dff9535b75fe639fd23c6f1763
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-27 18:33:18 -0400

    Validate input from using useReducer

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from  'react';
+import React, { useState, useReducer } from  'react';
     
 const formStyle={
     width: '50%',
@@ -10,51 +10,104 @@ const formStyle={
 const errorStyle={
     color: 'red'
 }
-const UserForm = (props) => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");  
-    const [confirmPassword, setConfirmPassword] = useState("");  
+const initialState = {
+        firstName: {
+            value: '',
+            error: null
+        },
+        lastName: {
+            value: '',
+            error: null
+        },
+        email: {
+            value: '',
+            error: null
+        },
+    password: {
+            value: '',
+            error: null
+        },
+    confirmPassword: {
+            value: '',
+            error: null
+        },
+
+    };
     
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { firstName,lastName, email, password };
-        console.log("Welcome", newUser);
-        setFirstName("")
-        setLastName("")
-        setEmail("")
-        setPassword("")
-        setConfirmPassword("")
-    };
+const reducer = (state = initialState, action) => {
+const {type, payload} = action;
+console.log(type, " this is type")
+switch(type) {
+    case "firstName":
+        console.log(state.firstName.value) 
+        if(payload.length < 2){
+            return {...state, [type]:{value: payload, error: "First Name must be at least 2 Character"}};
+        }
+        return {...state, [type]:{value: payload}};
+    case "lastName":
+        if(payload.length < 2){
+            return {...state, [type]:{value: payload, error: "Last Name must be at least 2 Character"}};
+        } 
+        return {...state, [type]:{value: payload}};
+    case "email":
+        if(payload.length < 8){
+            return {...state, [type]:{value: payload, error: "Email must be at least 8 Character"}};
+        } 
+        return {...state, [type]:{value: payload}};
+    case "password":
+        if(payload.length < 8){
+            return {...state, [type]:{value: payload, error: "Password must be at least 8 Character"}};
+        } 
+        return {...state, [type]:{value: payload}};
+    case "confirmPassword":
+        if(payload.length < 8){
+            return {...state, [type]:{value: payload, error: "Password must be at least 8 Character"}};
+        }
+        if(payload !== state.password.value){
+            return {...state, [type]:{value: payload, error: "Password must be match"}};
+        } 
+        return {...state, [type]:{value: payload}};
+    default:
+        throw new Error();
+}
+}
+const UserForm = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        dispatch({
+            type: name,
+            payload: value
+        })
+    }
     return(
     <div style={{width: '70%', margin: '0 auto'}}>
-        <form style={formStyle} onSubmit={ createUser }>
+        <form style={formStyle}>
             <label>First Name: </label>
-            <input type="text" onChange={ (e) => setFirstName(e.target.value) } value={firstName}/>
-            {firstName.length> 0 && firstName.length < 2 && <p style={errorStyle}>First name must be at least 2 Characters</p>}
+            <input name="firstName" type="text" onChange={handleChange} value={state.firstName.value}/>
+            {state.firstName.error != null && <p style={errorStyle}>{state.firstName.error}</p>}
             <label>Last Name: </label> 
-            <input type="text" onChange={ (e) => setLastName(e.target.value) } value={lastName}/>
-            {lastName.length> 0 && lastName.length < 2 && <p style={errorStyle}>Last name must be at least 2 Characters</p>}
+            <input name="lastName" type="text" onChange={handleChange} value={state.lastName.value}/>
+            {state.lastName.error != null && <p style={errorStyle}>{state.lastName.error}</p>}
             <label>Email Address: </label> 
-            <input type="text" onChange={ (e) => setEmail(e.target.value) } value={email}/>
-            {email.length > 0 && email.length < 5 && <p style={errorStyle}>email must be at least 5 Characters</p>}
+            <input name="email" type="text" onChange={handleChange} value={state.email.value}/>
+            {state.email.error != null && <p style={errorStyle}>{state.email.error}</p>}
             <label>Password: </label>
-            <input type="text" onChange={ (e) => setPassword(e.target.value) } value={password}/>
-            {password.length > 0 && password.length < 8 && <p style={errorStyle}>Password must be at least 8 Characters</p>}
+            <input name="password" type="password" onChange={handleChange} value={state.password.value}/>
+            {state.password.error != null && <p style={errorStyle}>{state.password.error}</p>}
             <label>Confirm Password: </label>
-            <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } value={confirmPassword}/>
-            {confirmPassword.length > 0 && confirmPassword.length < 8 && <p style={errorStyle}>Confirm Password must be at least 8 Characters</p>}
-            {confirmPassword.length > 0 && password !== confirmPassword && <p style={errorStyle}>Password must match </p>}            
+            <input name="confirmPassword" type="password" onChange={handleChange} value={state.confirmPassword.value}/>
+            {state.confirmPassword.error != null && <p style={errorStyle}>{state.confirmPassword.error}</p>}
             <input type="submit" value="Create User" />
         </form>
 <h2>Your Form Data</h2>
 <div>
-<p>First Name: {firstName}</p>
-<p>Last Name: {lastName}</p>
-<p>Email: {email}</p>
-<p>Password: {password}</p>
-<p>Confirm Password: {confirmPassword}</p>
+<p>First Name: {state.firstName.value}</p>
+<p>Last Name: {state.lastName.value}</p>
+<p>Email: {state.email.value}</p>
+<p>Password: {state.password.value}</p>
+<p>Confirm Password: {state.confirmPassword.value}</p>
 </div>
     </div>
 
